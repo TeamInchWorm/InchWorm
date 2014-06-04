eebd6fdc9dc09d0670fc142f3a5ead0999b190ae
BabyMaybe <Eric_Varela@mac.com>
2014-06-04 01:29:55 -0400

    Lots of glue
    Hooking everything up. Score is broadcast in the ScoreChange event.
    Time is broadcast in the TimeChange event. GameStart event called on
    ManagerStart and inside of Play in the AudioManager to reset the clock.
    Also added a distance traveled script to avoid messing up anything in
    your worm scripts.
    
    I’m a little tired right now, but everything should just work at this
    point. (systems anyways, all the actual events need to be added in
    still).

---
+++ Assets/DistanceTraveled.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistanceTraveled : MonoBehaviour {
+
+	private float xPos, xLast;
+	private GameEventManager GM;
+
+
+	// Use this for initialization
+	void Start () {
+		GM = GameObject.Find("Game Event Manager").GetComponent<GameEventManager>();
+		xPos = transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		xLast = xPos;
+		xPos = transform.position.x;
+
+		float dist = xPos - xLast;
+
+		if (dist != 0)
+			GM.ChangeScore(dist); 
+
+		else
+			GM.ChangeScore(-0.1f);			
+	}
+
+
+}

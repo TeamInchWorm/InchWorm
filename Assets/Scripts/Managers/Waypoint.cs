c44b4313f4c71b1eee238bdf9ef8d2ae7153058b
BabyMaybe <Eric_Varela@mac.com>
2014-06-06 20:19:24 -0400

    Friday night commit 1
    Set up Masks
    Added test overlays back in
    Added ability to set waypoints transition time\
    Locked camera y pos

--- Assets/Scripts/Managers/Waypoint.cs
+++ Assets/Scripts/Managers/Waypoint.cs
@@ -5,6 +5,7 @@ public class Waypoint : MonoBehaviour {
 
 	internal Vector3 position;
 	public float timeActivated;
+	public float transitionTime = 1.0f;
 
 	// Use this for initialization
 	void Start () {
